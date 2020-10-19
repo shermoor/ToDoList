f664fbf3dfd10f70a76ee6be4a66782f09bc936d
Sherzot Ibragimov <jers.lic@icloud.com>
2020-10-19 23:54:39 +0600

    Initial commit

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    private let realm = try! Realm()
+    
+    private var data = [ToDoListItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "To Do List"
     }
-
-
+    
+    // MARK: Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Open the screen where we can see item info or delete
+        
+        let item = data[indexPath.row]
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "view") as? ViewViewController else { return }
+        
+        vc.item = item
+        vc.deletionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.title = item.item
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func didTappAddButton() {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "enter") as? EntryViewController else { return }
+        
+        vc.completionHandler = { [weak self] in
+            self?.refresh()
+        }
+        vc.title = "New Item"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func refresh() {
+        data = realm.objects(ToDoListItem.self).map({ $0 })
+        table.reloadData()
+    }
 }
 
---
+++ ToDoList/ViewViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewViewController.swift
+//  ToDoList
+//
+//  Created by Sherzot Ibragimov on 10/17/20.
+//
+import RealmSwift
+import UIKit
+
+class ViewViewController: UIViewController {
+    
+    public var item: ToDoListItem?
+    public var deletionHandler: (() -> Void)?
+    
+    @IBOutlet var itemLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    private let realm = try! Realm()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        itemLabel.text = item?.item
+        dateLabel.text = Self.dateFormatter.string(from: item!.date)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(didTapDeleteButton))
+    }
+    
+    @objc private func didTapDeleteButton() {
+        guard let myItem = self.item else { return }
+        
+        realm.beginWrite()
+        realm.delete(myItem)
+        try! realm.commitWrite()
+        
+        deletionHandler?()
+        navigationController?.popToRootViewController(animated: true)
+    }
+}
